388aae7f18e101ef7d710005f8ec1fcbfcfb1224
arvee0220 <rowellverdejo@gmail.com>
2024-06-16 15:42:19 -0400

    Add patch method updateProduct by ID

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,10 +1,42 @@
 import Product from "../models/product.js";
 
+// Get all products
 const getProducts = async (_, res) => {
 	const products = await Product.find();
+	try {
+		if (!products) {
+			return res.status(404).json({
+				success: false,
+				message: "Database is empty",
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			products,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+
+// Get products by ID
+const getProductDetails = async (req, res) => {
+	console.log(req.params);
+	const product = await Product.findById(req.params.id);
+
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: "Product not found",
+		});
+	}
 
 	res.status(200).json({
-		products,
+		product,
 	});
 };
 
@@ -13,17 +45,36 @@ const newProduct = async (req, res) => {
 	const product = await Product.create(req.body);
 
 	res.status(200).json({
+		success: true,
 		product,
 	});
 };
 
-const getProductDetails = async (req, res) => {
-	console.log(req.params);
-	const product = await Product.findById(req.params.id);
+// Update Product
+const updateProduct = async ({ params: { id }, body }, res) => {
+	let product = await Product.findById(id);
 
-	res.status(200).json({
-		product,
-	});
+	try {
+		if (!product) {
+			return res.status(404).json({
+				error: "Product not found",
+			});
+		}
+
+		product = await Product.findByIdAndUpdate(id, body, {
+			new: true,
+		});
+
+		res.status(200).json({
+			success: true,
+			product,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
 };
 
-export { getProducts, newProduct, getProductDetails };
+export { getProducts, newProduct, getProductDetails, updateProduct };
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getProductDetails, getProducts, newProduct } from "../controllers/productControllers.js";
+import {
+	getProductDetails,
+	getProducts,
+	newProduct,
+	updateProduct,
+} from "../controllers/productControllers.js";
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/admin/products").post(newProduct);
 router.route("/products/:id").get(getProductDetails);
+router.route("/admin/products").post(newProduct);
+router.route("/products/:id").patch(updateProduct);
 
 export default router;
