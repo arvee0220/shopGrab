15c9c73c73a03d733a0bfdc9dcdac3fd1898e3d5
arvee0220 <rowellverdejo@gmail.com>
2024-06-23 17:22:04 -0400

    refactored search and filter functions

--- backend/utils/apiFilters.js
+++ backend/utils/apiFilters.js
@@ -1,30 +1,38 @@
-const search = (query, queryStr) => {
-	if (queryStr.keyword) {
-		const keyword = {
-			name: {
-				$regex: queryStr.keyword,
-				$options: "i",
-			},
-		};
-		query = query.find({ ...keyword });
-	}
+const apiFilters = (query, queryStr) => {
+	const search = () => {
+		if (queryStr.keyword) {
+			const keyword = {
+				name: {
+					$regex: queryStr.keyword,
+					$options: "i",
+				},
+			};
+			query = query.find({ ...keyword });
+		}
+
+		return query;
+	};
+
+	search();
+
+	const filters = () => {
+		const queryCopy = { ...queryStr };
+
+		const fieldsToRemove = ["keyword"];
+		fieldsToRemove.forEach((elem) => delete queryCopy[elem]);
+
+		// advance filters for price, ratings etc => check mongodb docs
+		let queryStrCopy = JSON.stringify(queryCopy);
+		queryStrCopy = queryStrCopy.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+		query = query.find(JSON.parse(queryStrCopy));
+
+		return query;
+	};
+
+	filters();
 
 	return query;
 };
 
-const filters = (query, queryStr) => {
-	const queryCopy = { ...queryStr };
-
-	const fieldsToRemove = ["keyword"];
-	fieldsToRemove.forEach((elem) => delete queryCopy[elem]);
-
-	// advance filters for price, ratings etc => check mongodb docs
-	let queryStrCopy = JSON.stringify(queryCopy);
-	queryStrCopy = queryStrCopy.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-
-	query = query.find(JSON.parse(queryStrCopy));
-
-	return query;
-};
-
-export { search, filters };
+export default apiFilters;
