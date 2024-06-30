3d073b3accf41139c6750ebb1c7cfe608d1c5177
arvee0220 <rowellverdejo@gmail.com>
2024-06-29 20:48:51 -0400

    Create login user api

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import catchAsyncErrors from "../middlewares/catchAsyncError.js";
 import User from "../models/user.js";
 import generateToken from "../utils/jwtToken.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 const registerUser = catchAsyncErrors(async (req, res, next) => {
 	const { name, email, password } = req.body;
@@ -35,10 +36,55 @@ const getUser = catchAsyncErrors(async (req, res, next) => {
 	const { id } = req.params;
 	const user = await User.findById(id);
 
-	res.status(200).json({
-		success: true,
-		user,
-	});
+	try {
+		if (!user) {
+			return next(ErrorHandler("User not found", 404));
+		}
+
+		res.status(200).json({
+			success: true,
+			user,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
 });
 
-export { registerUser, getUser };
+const loginUser = catchAsyncErrors(async (req, res, next) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email }).select("+password");
+	const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+	try {
+		if (!email || !password) {
+			return next(ErrorHandler("Please enter email & password"));
+		}
+
+		if (!user) {
+			return next(ErrorHandler("Invalid email or password", 401));
+		}
+
+		// Compare user password
+
+		if (!isPasswordMatched) {
+			return next(ErrorHandler("Invalid email or password", 401));
+		}
+
+		const token = generateToken(user);
+
+		res.status(201).json({
+			success: true,
+			token,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+});
+
+export { registerUser, getUser, loginUser };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser, getUser } from "../controllers/authControllers.js";
+import { registerUser, getUser, loginUser } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/user/:id", getUser);
 
 export default router;
